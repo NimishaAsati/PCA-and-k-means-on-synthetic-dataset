9016cabc0ec2622ebfd1fed665c7ff24f747c641
Nimisha Asati <43856552+NimishaAsati@users.noreply.github.com>
2019-09-14 11:01:20 -0500

    Add files via upload

---
+++ PCA and k-means.R
@@ -0,0 +1,478 @@
+# Creating the random dataset
+
+F1_A <- as.data.frame(runif(20,min = 10, max = 60))
+F1_B <- as.data.frame(runif(20,min = 60, max = 110))
+F1_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F1_A) <- "F1"
+colnames(F1_B) <- "F1"
+colnames(F1_C) <- "F1"
+F1 <- rbind(F1_A,F1_B,F1_C)
+
+F2_A <- as.data.frame(runif(20,min = 10, max = 60))
+F2_B <- as.data.frame(runif(20,min = 60, max = 110))
+F2_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F2_A) <- "F2"
+colnames(F2_B) <- "F2"
+colnames(F2_C) <- "F2"
+F2 <- rbind(F2_A,F2_B,F2_C)
+
+F3_A <- as.data.frame(runif(20,min = 10, max = 60))
+F3_B <- as.data.frame(runif(20,min = 60, max = 110))
+F3_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F3_A) <- "F3"
+colnames(F3_B) <- "F3"
+colnames(F3_C) <- "F3"
+F3 <- rbind(F3_A,F3_B,F3_C)
+
+F4_A <- as.data.frame(runif(20,min = 10, max = 60))
+F4_B <- as.data.frame(runif(20,min = 60, max = 110))
+F4_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F4_A) <- "F4"
+colnames(F4_B) <- "F4"
+colnames(F4_C) <- "F4"
+F4 <- rbind(F4_A,F4_B,F4_C)
+
+F5_A <- as.data.frame(runif(20,min = 10, max = 60))
+F5_B <- as.data.frame(runif(20,min = 60, max = 110))
+F5_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F5_A) <- "F5"
+colnames(F5_B) <- "F5"
+colnames(F5_C) <- "F5"
+F5 <- rbind(F5_A,F5_B,F5_C)
+
+F6_A <- as.data.frame(runif(20,min = 10, max = 60))
+F6_B <- as.data.frame(runif(20,min = 60, max = 110))
+F6_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F6_A) <- "F6"
+colnames(F6_B) <- "F6"
+colnames(F6_C) <- "F6"
+F6 <- rbind(F6_A,F6_B,F6_C)
+
+F7_A <- as.data.frame(runif(20,min = 10, max = 60))
+F7_B <- as.data.frame(runif(20,min = 60, max = 110))
+F7_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F7_A) <- "F7"
+colnames(F7_B) <- "F7"
+colnames(F7_C) <- "F7"
+F7 <- rbind(F7_A,F7_B,F7_C)
+
+F8_A <- as.data.frame(runif(20,min = 10, max = 60))
+F8_B <- as.data.frame(runif(20,min = 60, max = 110))
+F8_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F8_A) <- "F8"
+colnames(F8_B) <- "F8"
+colnames(F8_C) <- "F8"
+F8 <- rbind(F8_A,F8_B,F8_C)
+
+F9_A <- as.data.frame(runif(20,min = 10, max = 60))
+F9_B <- as.data.frame(runif(20,min = 60, max = 110))
+F9_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F9_A) <- "F9"
+colnames(F9_B) <- "F9"
+colnames(F9_C) <- "F9"
+F9 <- rbind(F9_A,F9_B,F9_C)
+
+F10_A <- as.data.frame(runif(20,min = 10, max = 60))
+F10_B <- as.data.frame(runif(20,min = 60, max = 110))
+F10_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F10_A) <- "F10"
+colnames(F10_B) <- "F10"
+colnames(F10_C) <- "F10"
+F10 <- rbind(F10_A,F10_B,F10_C)
+
+
+F11_A <- as.data.frame(runif(20,min = 10, max = 60))
+F11_B <- as.data.frame(runif(20,min = 60, max = 110))
+F11_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F11_A) <- "F11"
+colnames(F11_B) <- "F11"
+colnames(F11_C) <- "F11"
+F11 <- rbind(F11_A,F11_B,F11_C)
+
+F12_A <- as.data.frame(runif(20,min = 10, max = 60))
+F12_B <- as.data.frame(runif(20,min = 60, max = 110))
+F12_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F12_A) <- "F12"
+colnames(F12_B) <- "F12"
+colnames(F12_C) <- "F12"
+F12 <- rbind(F12_A,F12_B,F12_C)
+
+F13_A <- as.data.frame(runif(20,min = 10, max = 60))
+F13_B <- as.data.frame(runif(20,min = 60, max = 110))
+F13_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F13_A) <- "F13"
+colnames(F13_B) <- "F13"
+colnames(F13_C) <- "F13"
+F13 <- rbind(F13_A,F13_B,F13_C)
+
+F14_A <- as.data.frame(runif(20,min = 10, max = 60))
+F14_B <- as.data.frame(runif(20,min = 60, max = 110))
+F14_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F14_A) <- "F14"
+colnames(F14_B) <- "F14"
+colnames(F14_C) <- "F14"
+F14 <- rbind(F14_A,F14_B,F14_C)
+
+F15_A <- as.data.frame(runif(20,min = 10, max = 60))
+F15_B <- as.data.frame(runif(20,min = 60, max = 110))
+F15_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F15_A) <- "F15"
+colnames(F15_B) <- "F15"
+colnames(F15_C) <- "F15"
+F15 <- rbind(F15_A,F15_B,F15_C)
+
+F16_A <- as.data.frame(runif(20,min = 10, max = 60))
+F16_B <- as.data.frame(runif(20,min = 60, max = 110))
+F16_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F16_A) <- "F16"
+colnames(F16_B) <- "F16"
+colnames(F16_C) <- "F16"
+F16 <- rbind(F16_A,F16_B,F16_C)
+
+F17_A <- as.data.frame(runif(20,min = 10, max = 60))
+F17_B <- as.data.frame(runif(20,min = 60, max = 110))
+F17_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F17_A) <- "F17"
+colnames(F17_B) <- "F17"
+colnames(F17_C) <- "F17"
+F17 <- rbind(F17_A,F17_B,F17_C)
+
+F18_A <- as.data.frame(runif(20,min = 10, max = 60))
+F18_B <- as.data.frame(runif(20,min = 60, max = 110))
+F18_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F18_A) <- "F18"
+colnames(F18_B) <- "F18"
+colnames(F18_C) <- "F18"
+F18 <- rbind(F18_A,F18_B,F18_C)
+
+F19_A <- as.data.frame(runif(20,min = 10, max = 60))
+F19_B <- as.data.frame(runif(20,min = 60, max = 110))
+F19_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F19_A) <- "F19"
+colnames(F19_B) <- "F19"
+colnames(F19_C) <- "F19"
+F19 <- rbind(F19_A,F19_B,F19_C)
+
+F20_A <- as.data.frame(runif(20,min = 10, max = 60))
+F20_B <- as.data.frame(runif(20,min = 60, max = 110))
+F20_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F20_A) <- "F20"
+colnames(F20_B) <- "F20"
+colnames(F20_C) <- "F20"
+F20 <- rbind(F20_A,F20_B,F20_C)
+
+F21_A <- as.data.frame(runif(20,min = 10, max = 60))
+F21_B <- as.data.frame(runif(20,min = 60, max = 110))
+F21_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F21_A) <- "F21"
+colnames(F21_B) <- "F21"
+colnames(F21_C) <- "F21"
+F21 <- rbind(F21_A,F21_B,F21_C)
+
+F22_A <- as.data.frame(runif(20,min = 10, max = 60))
+F22_B <- as.data.frame(runif(20,min = 60, max = 110))
+F22_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F22_A) <- "F22"
+colnames(F22_B) <- "F22"
+colnames(F22_C) <- "F22"
+F22 <- rbind(F22_A,F22_B,F22_C)
+
+F23_A <- as.data.frame(runif(20,min = 10, max = 60))
+F23_B <- as.data.frame(runif(20,min = 60, max = 110))
+F23_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F23_A) <- "F23"
+colnames(F23_B) <- "F23"
+colnames(F23_C) <- "F23"
+F23 <- rbind(F23_A,F23_B,F23_C)
+
+F24_A <- as.data.frame(runif(20,min = 10, max = 60))
+F24_B <- as.data.frame(runif(20,min = 60, max = 110))
+F24_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F24_A) <- "F24"
+colnames(F24_B) <- "F24"
+colnames(F24_C) <- "F24"
+F24 <- rbind(F24_A,F24_B,F24_C)
+
+F25_A <- as.data.frame(runif(20,min = 10, max = 60))
+F25_B <- as.data.frame(runif(20,min = 60, max = 110))
+F25_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F25_A) <- "F25"
+colnames(F25_B) <- "F25"
+colnames(F25_C) <- "F25"
+F25 <- rbind(F25_A,F25_B,F25_C)
+
+F26_A <- as.data.frame(runif(20,min = 10, max = 60))
+F26_B <- as.data.frame(runif(20,min = 60, max = 110))
+F26_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F26_A) <- "F26"
+colnames(F26_B) <- "F26"
+colnames(F26_C) <- "F26"
+F26 <- rbind(F26_A,F26_B,F26_C)
+
+F27_A <- as.data.frame(runif(20,min = 10, max = 60))
+F27_B <- as.data.frame(runif(20,min = 60, max = 110))
+F27_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F27_A) <- "F27"
+colnames(F27_B) <- "F27"
+colnames(F27_C) <- "F27"
+F27 <- rbind(F27_A,F27_B,F27_C)
+
+F28_A <- as.data.frame(runif(20,min = 10, max = 60))
+F28_B <- as.data.frame(runif(20,min = 60, max = 110))
+F28_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F28_A) <- "F28"
+colnames(F28_B) <- "F28"
+colnames(F28_C) <- "F28"
+F28 <- rbind(F28_A,F28_B,F28_C)
+
+F29_A <- as.data.frame(runif(20,min = 10, max = 60))
+F29_B <- as.data.frame(runif(20,min = 60, max = 110))
+F29_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F29_A) <- "F29"
+colnames(F29_B) <- "F29"
+colnames(F29_C) <- "F29"
+F29 <- rbind(F29_A,F29_B,F29_C)
+
+F30_A <- as.data.frame(runif(20,min = 10, max = 60))
+F30_B <- as.data.frame(runif(20,min = 60, max = 110))
+F30_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F30_A) <- "F30"
+colnames(F30_B) <- "F30"
+colnames(F30_C) <- "F30"
+F30 <- rbind(F30_A,F30_B,F30_C)
+
+F31_A <- as.data.frame(runif(20,min = 10, max = 60))
+F31_B <- as.data.frame(runif(20,min = 60, max = 110))
+F31_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F31_A) <- "F31"
+colnames(F31_B) <- "F31"
+colnames(F31_C) <- "F31"
+F31 <- rbind(F31_A,F31_B,F31_C)
+
+F32_A <- as.data.frame(runif(20,min = 10, max = 60))
+F32_B <- as.data.frame(runif(20,min = 60, max = 110))
+F32_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F32_A) <- "F32"
+colnames(F32_B) <- "F32"
+colnames(F32_C) <- "F32"
+F32 <- rbind(F32_A,F32_B,F32_C)
+
+F33_A <- as.data.frame(runif(20,min = 10, max = 60))
+F33_B <- as.data.frame(runif(20,min = 60, max = 110))
+F33_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F33_A) <- "F33"
+colnames(F33_B) <- "F33"
+colnames(F33_C) <- "F33"
+F33 <- rbind(F33_A,F33_B,F33_C)
+
+F34_A <- as.data.frame(runif(20,min = 10, max = 60))
+F34_B <- as.data.frame(runif(20,min = 60, max = 110))
+F34_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F34_A) <- "F34"
+colnames(F34_B) <- "F34"
+colnames(F34_C) <- "F34"
+F34 <- rbind(F34_A,F34_B,F34_C)
+
+F35_A <- as.data.frame(runif(20,min = 10, max = 60))
+F35_B <- as.data.frame(runif(20,min = 60, max = 110))
+F35_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F35_A) <- "F35"
+colnames(F35_B) <- "F35"
+colnames(F35_C) <- "F35"
+F35 <- rbind(F35_A,F35_B,F35_C)
+
+F36_A <- as.data.frame(runif(20,min = 10, max = 60))
+F36_B <- as.data.frame(runif(20,min = 60, max = 110))
+F36_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F36_A) <- "F36"
+colnames(F36_B) <- "F36"
+colnames(F36_C) <- "F36"
+F36 <- rbind(F36_A,F36_B,F36_C)
+
+F37_A <- as.data.frame(runif(20,min = 10, max = 60))
+F37_B <- as.data.frame(runif(20,min = 60, max = 110))
+F37_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F37_A) <- "F37"
+colnames(F37_B) <- "F37"
+colnames(F37_C) <- "F37"
+F37 <- rbind(F37_A,F37_B,F37_C)
+
+F38_A <- as.data.frame(runif(20,min = 10, max = 60))
+F38_B <- as.data.frame(runif(20,min = 60, max = 110))
+F38_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F38_A) <- "F38"
+colnames(F38_B) <- "F38"
+colnames(F38_C) <- "F38"
+F38 <- rbind(F38_A,F38_B,F38_C)
+
+F39_A <- as.data.frame(runif(20,min = 10, max = 60))
+F39_B <- as.data.frame(runif(20,min = 60, max = 110))
+F39_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F39_A) <- "F39"
+colnames(F39_B) <- "F39"
+colnames(F39_C) <- "F39"
+F39 <- rbind(F39_A,F39_B,F39_C)
+
+F40_A <- as.data.frame(runif(20,min = 10, max = 60))
+F40_B <- as.data.frame(runif(20,min = 60, max = 110))
+F40_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F40_A) <- "F40"
+colnames(F40_B) <- "F40"
+colnames(F40_C) <- "F40"
+F40 <- rbind(F40_A,F40_B,F40_C)
+
+F41_A <- as.data.frame(runif(20,min = 10, max = 60))
+F41_B <- as.data.frame(runif(20,min = 60, max = 110))
+F41_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F41_A) <- "F41"
+colnames(F41_B) <- "F41"
+colnames(F41_C) <- "F41"
+F41 <- rbind(F41_A,F41_B,F41_C)
+
+F42_A <- as.data.frame(runif(20,min = 10, max = 60))
+F42_B <- as.data.frame(runif(20,min = 60, max = 110))
+F42_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F42_A) <- "F42"
+colnames(F42_B) <- "F42"
+colnames(F42_C) <- "F42"
+F42 <- rbind(F42_A,F42_B,F42_C)
+
+F43_A <- as.data.frame(runif(20,min = 10, max = 60))
+F43_B <- as.data.frame(runif(20,min = 60, max = 110))
+F43_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F43_A) <- "F43"
+colnames(F43_B) <- "F43"
+colnames(F43_C) <- "F43"
+F43 <- rbind(F43_A,F43_B,F43_C)
+
+F44_A <- as.data.frame(runif(20,min = 10, max = 60))
+F44_B <- as.data.frame(runif(20,min = 60, max = 110))
+F44_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F44_A) <- "F44"
+colnames(F44_B) <- "F44"
+colnames(F44_C) <- "F44"
+F44 <- rbind(F44_A,F44_B,F44_C)
+
+F45_A <- as.data.frame(runif(20,min = 10, max = 60))
+F45_B <- as.data.frame(runif(20,min = 60, max = 110))
+F45_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F45_A) <- "F45"
+colnames(F45_B) <- "F45"
+colnames(F45_C) <- "F45"
+F45 <- rbind(F45_A,F45_B,F45_C)
+
+F46_A <- as.data.frame(runif(20,min = 10, max = 60))
+F46_B <- as.data.frame(runif(20,min = 60, max = 110))
+F46_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F46_A) <- "F46"
+colnames(F46_B) <- "F46"
+colnames(F46_C) <- "F46"
+F46 <- rbind(F46_A,F46_B,F46_C)
+
+F47_A <- as.data.frame(runif(20,min = 10, max = 60))
+F47_B <- as.data.frame(runif(20,min = 60, max = 110))
+F47_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F47_A) <- "F47"
+colnames(F47_B) <- "F47"
+colnames(F47_C) <- "F47"
+F47 <- rbind(F47_A,F47_B,F47_C)
+
+F48_A <- as.data.frame(runif(20,min = 10, max = 60))
+F48_B <- as.data.frame(runif(20,min = 60, max = 110))
+F48_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F48_A) <- "F48"
+colnames(F48_B) <- "F48"
+colnames(F48_C) <- "F48"
+F48 <- rbind(F48_A,F48_B,F48_C)
+
+F49_A <- as.data.frame(runif(20,min = 10, max = 60))
+F49_B <- as.data.frame(runif(20,min = 60, max = 110))
+F49_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F49_A) <- "F49"
+colnames(F49_B) <- "F49"
+colnames(F49_C) <- "F49"
+F49 <- rbind(F49_A,F49_B,F49_C)
+
+F50_A <- as.data.frame(runif(20,min = 10, max = 60))
+F50_B <- as.data.frame(runif(20,min = 60, max = 110))
+F50_C <- as.data.frame(runif(20,min = 110, max = 160))
+colnames(F50_A) <- "F50"
+colnames(F50_B) <- "F50"
+colnames(F50_C) <- "F50"
+F50 <- rbind(F50_A,F50_B,F50_C)
+
+Q5_Pre_Data <- cbind(F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11,F12,F13,F14,F15,F16,F17,F18,F19,F20,F21,F22,F23,F24,F25,F26,F27,F28,F29,F30,F31,F32,F33,F34,F35,F36,F37,F38,F39,F40,F41,F42,F43,F44,F45,F46,F47,F48,F49,F50
+)
+
+df <- data.frame(m=LETTERS[1:3]) 
+label <- data.frame(df[rep(seq_len(nrow(df)), each=20),])
+colnames(label) <- "Label"
+
+Q5_Data <- cbind(label,Q5_Pre_Data)
+
+
+# ---------------------------------------------- Q5 (a) ----------------------------------------------
+
+# Running PCA on this data and finding the top 2 principal component axes
+library(factoextra)
+library("FactoMineR")
+Q5_pca <- PCA(Q5_Data[,-c(1)], scale.unit = TRUE, graph = FALSE)
+
+Q5_pca_eigen <- get_eigenvalue(Q5_pca)    # Finding Eigen values
+Q5_pca_eigen
+
+fviz_eig(Q5_pca, addlabels = TRUE, ylim = c(0, 150))    #Scree Plot
+
+fviz_pca_var(Q5_pca, col.var = "cos2",                                    # Plotting Variables PCA
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), 
+             repel = TRUE # Avoid text overlapping
+)
+
+var <- get_pca_var(Q5_pca)      # Storing PCA results
+var
+
+fviz_contrib(Q5_pca, choice = "var", axes = 1:2, top = 10)      # Finding the top contributors
+#Top 2 contributors are F4 and F45
+
+plot(Q5_Data$F4,Q5_Data$F45, col=factor(Q5_Data$Label))        # Plotting the top 2 contributors, coloring them based on initial labels
+# Since these 2 components have fairly segregated classes, we can proceed with this data for further questions
+
+# ---------------------------------------------- Q5 (b) ----------------------------------------------
+
+# Running K-means with K=3
+library(animation)
+set.seed(2345)
+kmean <- kmeans.ani(Q5_Data[,-c(1)], 3)
+
+#Comparing with the true class labels
+kmean$cluster
+table(kmean$cluster,Q5_Data$Label)
+
+
+# ---------------------------------------------- Q5 (c) ----------------------------------------------
+
+# Running K-means with K=4
+library(animation)
+set.seed(2345)
+kmean <- kmeans.ani(Q5_Data[,-c(1)], 4)
+
+# Running K-means with K=2
+library(animation)
+set.seed(2345)
+kmean <- kmeans.ani(Q5_Data[,-c(1)], 2)
+
+
+# ---------------------------------------------- Q5 (d) ----------------------------------------------
+
+# Running K-means with K=3 only on the 2 principal component axes obtained above from PCA
+library(animation)
+set.seed(2345)
+kmean <- kmeans.ani(Q5_Data[,c(5,46)], 3)      # Selecting F4 and F46 which are at column position 5 and 46 respectivelly
+
+
+# ---------------------------------------------- Q5 (e) ----------------------------------------------
+
+# Running K-means with K=3 after standardization
+library(animation)
+set.seed(2345)
+kmean <- kmeans.ani(scale(Q5_Data[,-c(1)]), 3)
